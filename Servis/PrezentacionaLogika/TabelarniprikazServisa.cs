b6a3c6ffd4c79a3de110d401e0ad8b92bfed5868
Bosko Vujisic <vujisicbosko@gmail.com>
2019-08-24 15:41:46 +0200

    Prezentaciona logika
    Dodat tabelarni prikaz servisa

---
+++ Servis/PrezentacionaLogika/TabelarniprikazServisa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KlasePodataka;
+
+namespace PrezentacionaLogika
+{
+    public class TabelarniprikazServisa
+    {
+        private string pStringKonekcije;
+
+        public TabelarniprikazServisa(string NoviStringKonekcije)
+        {
+            //pFilter="";
+            //pPodaciZaGrid = new DataSet();
+            pStringKonekcije = NoviStringKonekcije;
+        }
+
+        public DataSet DajPodatkeZaGrid(string Filter)
+        {
+            DataSet dsPodaci = new DataSet();
+            ServisDB objServisDB = new ServisDB(pStringKonekcije);
+            if (Filter.Equals(""))
+            {
+                dsPodaci = objServisDB.DajSveServise();
+            }
+            else
+            {
+                dsPodaci = objServisDB.DajServisePoID(Filter);
+            }
+            return dsPodaci;
+        }
+    }
+}
