f4f16cb9a806564fbaf58c250a7b7d30169f2fed
Nemanja Krasic <nemanjakrasic187@gmail.com>
2019-08-24 15:46:13 +0200

    Frontend
    Kreiranje frontend-a

---
+++ Servis/WebSite2/Default.aspx.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.DynamicData;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+
+    }
+
+}
