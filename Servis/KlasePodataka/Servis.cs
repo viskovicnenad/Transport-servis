501412e8715239f4e7f416836eca1f50b3bf5561
Bosko Vujisic <vujisicbosko@gmail.com>
2019-08-24 15:39:13 +0200

    Klase Podataka
    Dodata je klasa servis za potrebe projekta

---
+++ Servis/KlasePodataka/Servis.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace KlasePodataka
+{
+    public class Servis
+    {
+        // atributi
+        private string pID;
+        private int pCena;
+        private string pIme;
+        private string pIdDrzave;
+        private string pIdKorisnika;
+
+        //konstruktor
+        public Servis()
+        {
+
+        }
+
+        // javne metode
+        public string ID
+        {
+            get { return pID; }
+            set { pID = value; }
+        }
+
+        public int Cena
+        {
+            get { return pCena; }
+            set { pCena = value; }
+        }
+        public string Ime
+        {
+            get { return pIme; }
+            set { pIme = value; }
+        }
+        public string IdDrzave
+        {
+            get { return pIdDrzave; }
+            set { pIdDrzave = value; }
+        }
+
+        public string IdKorisnika
+        {
+            get { return pIdKorisnika; }
+            set { pIdKorisnika = value; }
+        }
+    }
+}
